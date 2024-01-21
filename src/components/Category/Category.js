ce39db5a7fe18a347a8156e92d0fd22070b663c7
Gaurang-Singhania <gaurang936@gmail.com>
2024-01-21 15:16:26 +0530

    Added Pagination

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -2,9 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import './category.css';
 
-
 const Category = ({ category }) => {
-  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -16,37 +14,70 @@ const Category = ({ category }) => {
         const data = await response.json();
         setProducts(data.products);
       } catch (error) {
-        console.error(`Error fetching ${category} data:`, error); 
+        console.error(`Error fetching ${category} data:`, error);
       }
     };
 
     fetchProducts();
   }, [category]);
 
+  const handleAddToCart = (productId) => {
+    console.log(`Product added to cart: ${productId}`);
+  };
+
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 2;
+  const lastIndex = currentPage * productsPerPage;
+  const firstIndex = lastIndex - productsPerPage;
+  const records = products.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(products.length / productsPerPage);
+  const numbers = [...Array(npage).keys()].map((number) => number + 1);
+
   return (
-    <div>
-      <h2>{category}</h2>
-      {Array.isArray(products) ? (
-        products.map((product) => (
-          <div key={product.id} className='item'>
+    <div className='category-grid'>
+      {Array.isArray(records) ? (
+        records.map((product) => (
+          <div key={product.id} className='item1'>
             <img src={product.thumbnail} alt={product.title} />
             <p>{product.name}</p>
-            <div className='item-prices'>
-              <div className='item-price-new'>
+            <div className='brand-name'>
+              <p><u>Brand:</u> {product.brand}</p>
+            </div>
+            <div className='title-desc'>
+              <p>Title: {product.title}</p>
+              <p>Description: {product.description}</p>
+            </div>
+            <div className='item1-prices'>
+              <div className='item1-price-new'>
                 ${product.price}
               </div>
-              <div className='item-rating'>
+              <div className='item1-rating'>
                 Rating: {product.rating}
               </div>
-              <div className='item-discount'>
-                {product.discount}% off
+              <div className='item1-discount'>
+                {product.discountPercentage}% off
               </div>
             </div>
+            <button className='cartButton' onClick={() => handleAddToCart(product.id)}>Add to Cart</button>
           </div>
         ))
       ) : (
         <p>No products available</p>
       )}
+      <div className='pagination'>
+        <p>Pages: </p>
+        {numbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => setCurrentPage(number)}
+            className={`pagination-button ${currentPage === number ? 'active' : ''}`}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
+
     </div>
   );
 };
